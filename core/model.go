b1ae292b0c61c91ecdb4950b7a67e0d24f31e213
Akalanka Perera <akalankaperera128@gmail.com>
2024-04-21 16:42:17 +0530

    feat: added support for chaining and operations

--- core/model.go
+++ core/model.go
@@ -158,7 +158,26 @@ func (m Model[T]) addToPipeline(stage, key string, value any) Model[T] {
 		filters := qkit.Cast[primitive.M](e_utils.CastBSON[bson.M](stg)[stage])
 		if filters != nil {
 			foundMatchStage = true
-			filters[m.whereField] = primitive.M{key: value}
+			filterExistsWithinAndOperator := false
+			if filters["$and"] != nil {
+				for _, filter := range filters["$and"].([]primitive.M) {
+					if filter[m.whereField] != nil {
+						filterExistsWithinAndOperator = true
+						filters["$and"] = append(filters["$and"].([]primitive.M), primitive.M{m.whereField: primitive.M{key: value}})
+					}
+				}
+			}
+			if !filterExistsWithinAndOperator {
+				if filters[m.whereField] == nil {
+					filters[m.whereField] = primitive.M{key: value}
+				} else {
+					filters["$and"] = []primitive.M{
+						{m.whereField: filters[m.whereField]},
+						{m.whereField: primitive.M{key: value}},
+					}
+					delete(filters, m.whereField)
+				}
+			}
 			return bson.D{{Key: stage, Value: filters}}
 		}
 		return stg
