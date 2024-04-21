72fd4f4155e1f0b4df54ac335d1517c9f5b2c28a
Akalanka Perera <akalankaperera128@gmail.com>
2024-04-21 20:07:17 +0530

    feat: added model is function

---
+++ tests/setup.go
@@ -0,0 +1,44 @@
+package e_tests
+
+import (
+	"elemental/core"
+	"reflect"
+	"time"
+
+	"github.com/clubpay/qlubkit-go"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type User struct {
+	ID         primitive.ObjectID `json:"_id" bson:"_id"`
+	Name       string             `json:"name" bson:"name"`
+	Age        int                `json:"age" bson:"age"`
+	Occupation string             `json:"occupation" bson:"occupation,omitempty"`
+	Weapons    []string           `json:"weapons" bson:"weapons"`
+	CreatedAt  time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt  time.Time          `json:"updated_at" bson:"updated_at"`
+}
+
+var UserModel = elemental.NewModel[User]("User", elemental.NewSchema(map[string]elemental.Field{
+	"Name": {
+		Type:     reflect.String,
+		Required: true,
+		Index: options.IndexOptions{
+			Unique: qkit.ValPtr(true),
+		},
+	},
+	"Age": {
+		Type:    reflect.Int,
+		Default: 18,
+	},
+	"Occupation": {
+		Type: reflect.String,
+	},
+	"Weapons": {
+		Type:    reflect.Slice,
+		Default: []string{},
+	},
+}, elemental.SchemaOptions{
+	Collection: "users",
+}))
