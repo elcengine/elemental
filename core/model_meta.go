06a28b31082d1a8eb7c349d0b55da1e95475d74b
Akalanka Perera <akalankaperera128@gmail.com>
2024-04-22 11:29:55 +0530

    feat: added collection stats

--- core/model_meta.go
+++ core/model_meta.go
@@ -4,16 +4,42 @@ import (
 	"context"
 	"elemental/connection"
 	"elemental/utils"
-
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
-
 
 func (m Model[T]) Collection() *mongo.Collection {
 	return e_connection.Use(m.schema.Options.Database, m.schema.Options.Connection).Collection(m.schema.Options.Collection)
 }
 
+func (m Model[T]) Database() *mongo.Database {
+	return m.Collection().Database()
+}
+
 func (m Model[T]) EstimatedDocumentCount(ctx ...context.Context) int64 {
 	count, _ := m.Collection().EstimatedDocumentCount(e_utils.DefaultCTX(ctx))
 	return count
+}
+
+func (m Model[T]) Stats(ctx ...context.Context) CollectionStats {
+	result := m.Database().RunCommand(e_utils.DefaultCTX(ctx), bson.M{"collStats": m.schema.Options.Collection})
+	var stats CollectionStats
+	e_utils.Must(result.Decode(&stats))
+	return stats
+}
+
+func (m Model[T]) TotalSize(ctx ...context.Context) int64 {
+	return m.Stats(e_utils.DefaultCTX(ctx)).Size
+}
+
+func (m Model[T]) StorageSize(ctx ...context.Context) int64 {
+	return m.Stats(e_utils.DefaultCTX(ctx)).StorageSize
+}
+
+func (m Model[T]) TotalIndexSize(ctx ...context.Context) int64 {
+	return m.Stats(e_utils.DefaultCTX(ctx)).TotalIndexSize
+}
+
+func (m Model[T]) AvgObjSize(ctx ...context.Context) int64 {
+	return m.Stats(e_utils.DefaultCTX(ctx)).AvgObjSize
 }
--- tests/core_meta_test.go
+++ tests/core_meta_test.go
@@ -20,5 +20,30 @@ func TestCoreMeta(t *testing.T) {
 			count := UserModel.EstimatedDocumentCount()
 			So(count, ShouldEqual, len(e_mocks.Users))
 		})
+		Convey("Stats", func() {
+			Convey("As a whole", func() {
+				stats := UserModel.Stats()
+				So(stats.Count, ShouldEqual, len(e_mocks.Users))
+				So(stats.AvgObjSize, ShouldBeGreaterThan, 0)
+				So(stats.Size, ShouldBeGreaterThan, 0)
+				So(stats.StorageSize, ShouldBeGreaterThan, 0)
+			})
+			Convey("Just the total size", func() {
+				size := UserModel.TotalSize()
+				So(size, ShouldBeGreaterThan, 0)
+			})
+			Convey("Just the storage size", func() {
+				size := UserModel.StorageSize()
+				So(size, ShouldBeGreaterThan, 0)
+			})
+			Convey("Just the total index size", func() {
+				size := UserModel.TotalIndexSize()
+				So(size, ShouldBeGreaterThan, 0)
+			})
+			Convey("Just the average object size", func() {
+				size := UserModel.AvgObjSize()
+				So(size, ShouldBeGreaterThan, 0)
+			})
+		})
 	})
 }
