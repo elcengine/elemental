ce80a1de245ed244395da0c79106358ae1cd3e09
Laksara <it21092824@my.sliit.lk>
2024-05-02 21:57:50 +0530

    feat: initiated command interfaces

---
+++ cmd/migrate.go
@@ -0,0 +1,15 @@
+package e_cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var migrateCmd = &cobra.Command{
+	Use:   "migrate",
+	Short: "Run database migrations",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Running database migrations")
+	},
+}
---
+++ cmd/root.go
@@ -0,0 +1,39 @@
+package e_cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "elemental",
+	Short: "Your next gen database ODM",
+	Long: `Elemental is a user database ODM that allows you to interact with your database in a much more user friendly way than standard database drivers`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(`
+
+Welcome to Elemental!.
+
+------------------------------------		
+
+Please run 'elemental --help' to see available commands.
+
+If you encounter any issues, please report them at "https://github.com/go-elemental/elemental/issues"
+
+------------------------------------`)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(migrateCmd)
+	rootCmd.AddCommand(seedCmd)
+  }
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
