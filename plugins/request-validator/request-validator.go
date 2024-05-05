474024d678080da5811792e4c101b56416b92732
Laksara <it21092824@my.sliit.lk>
2024-05-06 00:20:33 +0530

    updated request validator demo

--- plugins/request-validator/request-validator.go
+++ plugins/request-validator/request-validator.go
@@ -5,12 +5,11 @@ import (
 	"reflect"
 	"strconv"
 	"strings"
-	"elemental/tests/data"
 )
 type User struct {
-	ID       int    `validate:"exists=user_table"`
-	Name     string `validate:"exists=user_table"`
-	Age      int    `validate:"IsGreater=18"`
+	ID       int    `validate:"exists=user_table,UserId"`
+	Name     string `validate:"exists=user_table,Name"`
+	Age      int    `validate:"IsGreater=20"`
 	IsActive bool   `validate:"isTrue"`
 }
 
@@ -20,67 +19,76 @@ func ValidateStructWithDB(input interface{}) error {
 	v := reflect.ValueOf(input)
 	for i := 0; i < v.NumField(); i++ {
 		field := v.Type().Field(i)
-		// value := v.Field(i).Interface()	
+		value := fmt.Sprintf("%v", v.Field(i))
 		tag := strings.Split(field.Tag.Get("validate"), "=")
-		params := strings.Split(tag[1], ",")
-			switch tag[0] {
+		switch tag[0] {
 			case "exists":
+				params := strings.Split(tag[1], ",")
 				table := params[0]
 				column := params[1]
 				// Perform database lookup to check existence
-				// If not exists, return error
-				if input != nil {
-					fieldValue := reflect.ValueOf(input).FieldByName(field.Name)
-					if !checkExistence( column, fieldValue.Interface()) {
-						return fmt.Errorf("%s does not exist in %s", column, table)
-					}
-				}
+
+				// if input != nil {
+				// 	fieldValue := reflect.ValueOf(input).FieldByName(field.Name)
+				// 	if !checkExistence( column, fieldValue.Interface()) {
+				// 		return fmt.Errorf("%s does not exist in %s", column, table)
+				// 	}
+				// }
+				// fmt.Println("Checking if the %v exists in the database")
+				fmt.Printf("Checking if the %s with value %s exists in the %s table, column %s\n", field.Name, value, table, column)
+				// return nil
 
 			case "IsGreater":
+				params := strings.Split(tag[1], ",")
 				threshold, _ := strconv.Atoi(params[0])
 				//chek if the dataset has a larger value
-				if !checkIsGreater(threshold, field.Name) {
-					return fmt.Errorf("%s is not greater than %d", field.Name, threshold)
-				}
+
+				// if !checkIsGreater(threshold, field.Name) {
+				// 	return fmt.Errorf("%s is not greater than %d", field.Name, threshold)
+				// }
+				fmt.Printf("Checking if the %s is greater than %d\n", field.Name, threshold)
+				// return nil
 
 			case "isTrue":
 				//pass the value of the field to checkIsTrue function
-				key, _ := strconv.Atoi(v.Field(0).Interface().(string))
-				if !checkIsTrue(field.Name, key) {
-					return fmt.Errorf("%s is not true", field.Name)
-				}
-				
+
+				// key, _ := strconv.Atoi(v.Field(0).Interface().(string))
+				// if !checkIsTrue(field.Name, key) {
+				// 	return fmt.Errorf("%s is not true", field.Name)
+				// }
+				fmt.Printf("Checking if the %s is true\n", field.Name)
+				// return nil
 			}
 		
 	}
 	return nil
 }
 
-var users = data.Users
+// var users = data.Users
 
-func checkExistence( column string, value interface{}) bool {	
-	for _, user := range users {
-		if reflect.ValueOf(user).FieldByName(column).Interface() == value {
-			return true
-		}
-	}
-	return false
-}
+// func checkExistence( column string, value interface{}) bool {	
+// 	for _, user := range users {
+// 		if reflect.ValueOf(user).FieldByName(column).Interface() == value {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
 
-func checkIsGreater(threshold int, feildName string) bool {
-	for _, user := range users {
-		if reflect.ValueOf(user).FieldByName(feildName).Interface().(int) > threshold {
-			return true
-		}
-	}
-	return false
-}
+// func checkIsGreater(threshold int, feildName string) bool {
+// 	for _, user := range users {
+// 		if reflect.ValueOf(user).FieldByName(feildName).Interface().(int) > threshold {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
 
-func checkIsTrue(feildName string, id int) bool {
-	for _, user := range users {
-		if user.ID == id && reflect.ValueOf(user).FieldByName(feildName).Interface().(bool){
-			return true
-		}
-	}
-	return false
-}
+// func checkIsTrue(feildName string, id int) bool {
+// 	for _, user := range users {
+// 		if user.ID == id && reflect.ValueOf(user).FieldByName(feildName).Interface().(bool){
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
