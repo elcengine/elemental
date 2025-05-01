2a38c4504617b15429c4407e1ec56a356421a3ba
Akalanka Perera <akalankaperera128@gmail.com>
2025-05-01 21:05:11 +0530

    test: fixed flaky audit test

--- tests/core_audit_test.go
+++ tests/core_audit_test.go
@@ -3,12 +3,11 @@ package e_tests
 import (
 	"reflect"
 	"testing"
-	"time"
 
+	"github.com/akalanka47000/go-modkit/parallel_convey"
 	"github.com/elcengine/elemental/core"
 	"github.com/elcengine/elemental/tests/setup"
 	"github.com/elcengine/elemental/utils"
-	"github.com/akalanka47000/go-modkit/parallel_convey"
 	. "github.com/smartystreets/goconvey/convey"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -29,40 +28,43 @@ func TestCoreAudit(t *testing.T) {
 
 	KingdomModel.EnableAuditing()
 
+	AuditModel := elemental.AuditModel.SetDatabase(t.Name())
+
 	Convey("Inspect audit records", t, func() {
 		ParallelConvey, Wait := pc.New(t)
 
 		ParallelConvey("Insert", func() {
 			KingdomModel.Create(Kingdom{Name: "Nilfgaard"}).Exec()
 			SoTimeout(t, func() (ok bool) {
-				audit := e_utils.Cast[elemental.Audit](elemental.AuditModel.FindOne(primitive.M{"entity": entity, "type": elemental.AuditTypeInsert}).Exec())
+				audit := e_utils.Cast[elemental.Audit](AuditModel.FindOne(primitive.M{"entity": entity, "type": elemental.AuditTypeInsert}).Exec())
 				if audit.Type != "" {
 					ok = true
 				}
 				return
-			}, time.After(15*time.Second))
+			})
 		})
 
 		ParallelConvey("Update", func() {
 			KingdomModel.UpdateOne(&primitive.M{"name": "Nilfgaard"}, Kingdom{Name: "Redania"}).Exec()
 			SoTimeout(t, func() (ok bool) {
-				audit := e_utils.Cast[elemental.Audit](elemental.AuditModel.FindOne(primitive.M{"entity": entity, "type": elemental.AuditTypeUpdate}).Exec())
+				audit := e_utils.Cast[elemental.Audit](AuditModel.FindOne(primitive.M{"entity": entity, "type": elemental.AuditTypeUpdate}).Exec())
 				if audit.Type != "" {
 					ok = true
 				}
 				return
-			}, time.After(15*time.Second))
+			})
 		})
-		
+
 		ParallelConvey("Delete", func() {
-			KingdomModel.DeleteOne(primitive.M{"name": "Redania"}).Exec()
+			KingdomModel.Create(Kingdom{Name: "Skellige"}).Exec()
+			KingdomModel.DeleteOne(primitive.M{"name": "Skellige"}).Exec()
 			SoTimeout(t, func() (ok bool) {
-				audit := e_utils.Cast[elemental.Audit](elemental.AuditModel.FindOne(primitive.M{"entity": entity, "type": elemental.AuditTypeDelete}).Exec())
+				audit := e_utils.Cast[elemental.Audit](AuditModel.FindOne(primitive.M{"entity": entity, "type": elemental.AuditTypeDelete}).Exec())
 				if audit.Type != "" {
 					ok = true
 				}
 				return
-			}, time.After(15*time.Second))
+			})
 		})
 
 		Wait()
