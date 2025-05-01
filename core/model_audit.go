d21874f30555dfa38840070ec0cf28fdd185e6a6
Akalanka Perera <akalankaperera128@gmail.com>
2025-05-01 17:36:49 +0530

    fix: bug in audit when auditing against custom dbs/colls/clients

--- core/model_audit.go
+++ core/model_audit.go
@@ -21,7 +21,7 @@ type Audit struct {
 	Entity    string             `json:"entity" bson:"entity"`
 	Type      AuditType          `json:"type" bson:"type"`
 	Document  primitive.M        `json:"document" bson:"document"`
-	User      string            `json:"user" bson:"user"`
+	User      string             `json:"user" bson:"user"`
 	CreatedAt primitive.DateTime `json:"created_at" bson:"created_at"`
 	UpdatedAt primitive.DateTime `json:"updated_at" bson:"updated_at"`
 }
@@ -47,28 +47,42 @@ var AuditModel = NewModel[Audit]("Audit", NewSchema(map[string]Field{
 
 func (m Model[T]) EnableAuditing(ctx ...context.Context) {
 	context := e_utils.DefaultCTX(ctx)
+
+	execWithModelOpts := func(q Model[Audit]) {
+		if m.temporaryConnection != nil {
+			q = q.SetConnection(*m.temporaryConnection)
+		}
+		if m.temporaryDatabase != nil {
+			q = q.SetDatabase(*m.temporaryDatabase)
+		}
+		if m.temporaryCollection != nil {
+			q = q.SetCollection(*m.temporaryCollection)
+		}
+		q.Exec(context)
+	}
+
 	m.OnInsert(func(doc T) {
-		AuditModel.Create(Audit{
+		execWithModelOpts(AuditModel.Create(Audit{
 			Entity:   m.Name,
 			Type:     AuditTypeInsert,
 			Document: *e_utils.ToBSONDoc(doc),
 			User:     e_utils.Cast[string](context.Value(e_constants.CTXUser)),
-		}).Exec(context)
+		}))
 	}, TriggerOptions{Context: &context, Filter: &primitive.M{"ns.coll": primitive.M{"$eq": m.Collection().Name()}}})
 	m.OnUpdate(func(doc T) {
-		AuditModel.Create(Audit{
+		execWithModelOpts(AuditModel.Create(Audit{
 			Entity:   m.Name,
 			Type:     AuditTypeUpdate,
 			Document: *e_utils.ToBSONDoc(doc),
 			User:     e_utils.Cast[string](context.Value(e_constants.CTXUser)),
-		}).Exec(context)
+		}))
 	}, TriggerOptions{Context: &context, Filter: &primitive.M{"ns.coll": primitive.M{"$eq": m.Collection().Name()}}})
 	m.OnDelete(func(id primitive.ObjectID) {
-		AuditModel.Create(Audit{
+		execWithModelOpts(AuditModel.Create(Audit{
 			Entity:   m.Name,
 			Type:     AuditTypeDelete,
 			Document: map[string]interface{}{"_id": id},
 			User:     e_utils.Cast[string](context.Value(e_constants.CTXUser)),
-		}).Exec(context)
+		}))
 	}, TriggerOptions{Context: &context, Filter: &primitive.M{"ns.coll": primitive.M{"$eq": m.Collection().Name()}}})
 }
